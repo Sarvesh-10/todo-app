2935307d8e0020b57fcbcfcd4006cbc7e04e79bb
SSIDDHARTHAS <srajawat@avaya.com>
2024-07-22 22:58:43 +0530

    add: basic todo list storage using state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = () => {
+    const textarea = document.querySelector('textarea');
+    const todo = textarea.value;
+    if (todo) setTodoList([...todoList, todo]);
+    textarea.value = '';
+  }
+
   return (
     <>
       <textarea placeholder='Todo Item'></textarea>
-      <button>Add</button>
+      <button onClick={addTodo}>Add</button>
+      {todoList.map((todo, id) => <div key={id}>{todo}</div>)}
     </>
   );
 }
